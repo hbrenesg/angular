4cac14448dd76d84858287e4a3e4087dd841539f
hbrenesg <hbrenesg@ucenfotec.ac.cr>
2023-10-02 18:06:19 -0600

    respaldo de la version 2 del proyecto

---
+++ respaldoProyecto/version 2/proyectoHermes/src/app/components/resena/resena.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resena',
+  templateUrl: './resena.component.html',
+  styleUrls: ['./resena.component.css']
+})
+export class ResenaComponent {
+
+}
