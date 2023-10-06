2b33160fffab4ac0ba4c8c94cb52bd6c6daef1ff
hbrenesg <hbrenesg@ucenfotec.ac.cr>
2023-10-05 22:45:11 -0600

    mitad de clase 4

--- clase4/semana4/src/app/app.module.ts
+++ clase4/semana4/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule
