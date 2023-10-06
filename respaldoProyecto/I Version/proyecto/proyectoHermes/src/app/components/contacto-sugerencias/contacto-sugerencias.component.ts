c7f2af410390f94e16306c48db7fa98b59f6e21c
hbrenesg <hbrenesg@ucenfotec.ac.cr>
2023-10-06 09:14:26 -0600

    commit de compu Mac

---
+++ respaldoProyecto/I Version/proyecto/proyectoHermes/src/app/components/contacto-sugerencias/contacto-sugerencias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-sugerencias',
+  templateUrl: './contacto-sugerencias.component.html',
+  styleUrls: ['./contacto-sugerencias.component.css']
+})
+export class ContactoSugerenciasComponent {
+
+}
